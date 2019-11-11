c360787f88737286ae639bc3bba7bff47efce782
guoshengtao <57390871+guoshengtao@users.noreply.github.com>
2019-11-11 21:28:15 +0800

    Update and rename src/main/java/com/baizhi/CmfzGstApplication.java to  src/ main/ java/ com/ baizhi/CmfzGstApplication.java

--- src/ main/ java/ com/ baizhi/CmfzGstApplication.java
+++ src/ main/ java/ com/ baizhi/CmfzGstApplication.java
@@ -15,6 +15,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class CmfzGstApplication {
 
     public static void main(String[] args) {
+        System.out.println(".....................");
+        System.out.println(".....................");
         SpringApplication.run(CmfzGstApplication.class, args);
     }
 
